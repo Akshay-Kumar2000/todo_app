efd8a275fb0539d3daa37778c6b17b0fdb6ab94e
Akshay Kumar <93372936+Akshay-Kumar2000@users.noreply.github.com>
2022-03-27 17:47:11 +0530

    Add files via upload

---
+++ build/precache-manifest.42c63543447d22c858497c32e10aef50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01699e63be1ce7239bbfadcdb20ed57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cb709801119fa82be72",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "da9cc6ca4c91e93281b5",
+    "url": "/static/css/main.7570d1dc.chunk.css"
+  },
+  {
+    "revision": "2cb709801119fa82be72",
+    "url": "/static/js/2.9b263d7f.chunk.js"
+  },
+  {
+    "revision": "da9cc6ca4c91e93281b5",
+    "url": "/static/js/main.652a89d4.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  }
+]);
